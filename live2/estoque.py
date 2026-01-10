8377230b9fbfc83c19b13d777d65e46bcc1823a1
Gleydson Sena <grsena@gmail.com>
2026-01-10 08:43:29 -0300

    criando menu principal

---
+++ live2/estoque.py
@@ -0,0 +1,9 @@
+
+#Menu principal
+opcao_escolhida =""
+while (opcao_escolhida != "0"):
+    print("="*50)
+    print("7 - Cadastro Geral")
+    print("0 - Sair")
+    print("-"*50)
+    opcao_escolhida=input("Escolha uma das opções: ")
