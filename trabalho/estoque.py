24e93dbeca027c7f9fb7096fbf992b4f5b8be612
Gleydson Sena <grsena@gmail.com>
2026-01-11 16:41:53 -0300

    criando o menu

--- trabalho/estoque.py
+++ trabalho/estoque.py
@@ -1,15 +1,37 @@
+def selecionar_opcao(opcao):
+    print(opcao)
+    if opcao == "1":
+        adicionar_produto()
+    elif opcao == "2":
+        listar_produto()
+    elif opcao == "3":
+        atualizar_produto()
+    elif opcao == "4":
+        excluir_produto()
+    elif opcao == "0":
+        sair()
+
+else:
+
+    print("Opção inválida. Escolha uma opção do menu")
+
+    iniciar_sistema()
+
+
 
 def exibir_menu(): #Menu principal
-    opcao_escolhida =""
-    # while (opcao_escolhida != "0"):
     print("="*50)
     print("7 - Cadastro Geral")
     print("0 - Sair")
-    print("-"*50)
-    opcao_escolhida=input("Escolha uma das opções: ")
+    # print("-"*70)
+    
 
 
-# def iniciar_sistema:
-#     exibiir_menu()
+def iniciar_sistema():
+    exibir_menu()
+    opcao_escolhida = input("Escolha uma das opções: ")
+    selecionar_opcao(opcao_escolhida)
+    inicia_sistema() # mudar para um while para não ficar consumindo recurso.
 
-exibir_menu()
+
+iniciar_sistema()
