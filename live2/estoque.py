b8320e07648db640b9ee599c6c4c5d0ac565af16
Gleydson Sena <grsena@gmail.com>
2026-01-10 22:11:11 -0300

    Renomeia live2 para trabalho

--- live2/estoque.py
+++
@@ -1,9 +0,0 @@
-
-#Menu principal
-opcao_escolhida =""
-while (opcao_escolhida != "0"):
-    print("="*50)
-    print("7 - Cadastro Geral")
-    print("0 - Sair")
-    print("-"*50)
-    opcao_escolhida=input("Escolha uma das opções: ")
---
+++ trabalho/estoque.py
@@ -0,0 +1,15 @@
+
+def exibir_menu(): #Menu principal
+    opcao_escolhida =""
+    # while (opcao_escolhida != "0"):
+    print("="*50)
+    print("7 - Cadastro Geral")
+    print("0 - Sair")
+    print("-"*50)
+    opcao_escolhida=input("Escolha uma das opções: ")
+
+
+# def iniciar_sistema:
+#     exibiir_menu()
+
+exibir_menu()
