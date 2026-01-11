c91b316ddd1e8fc4b73559a30000cba56651015f
Gleydson Sena <grsena@gmail.com>
2026-01-11 20:01:26 -0300

    funcao itens do menus incluida

--- trabalho/estoque.py
+++ trabalho/estoque.py
@@ -3,7 +3,8 @@ from colorama import Fore, Back, Style
 # def sair():
 #     exit
 
-# def selecionar_opcao(opcao):
+ def selecionar_opcao(opcao):
+    print("ttessttee ---->")
 #     if opcao == "1":
 #         print("teste 01")
 #         # adicionar_produto()
@@ -24,21 +25,24 @@ from colorama import Fore, Back, Style
 
 
 def titulo_menu(titulo):
+    print()
     texto_central = ' ' + titulo +' '   # afastar o texto do menu das laterais 1 caracter de cada lado
-    largura_menu = 80                   # definir largura total em caracteres do titulo do menu
+    largura_menu = 70                   # definir largura total em caracteres do titulo do menu
 
     largura_texto_central = len(texto_central)                          # determina o tamanho do texto central do menu
     largura_lados_menu = (largura_menu - largura_texto_central) // 2    # determina o tamanho dos lados do menu
 
-    linha_menu = "#"*largura_lados_menu + texto_central + "#"*largura_lados_menu
-    print(Style.DIM + Fore.BLUE + linha_menu + Fore.RESET + Style.RESET_ALL)
+    linha_menu = '#'*largura_lados_menu + texto_central + '#'*largura_lados_menu    # texto a ser exibido no titulo menu
+    print(Fore.BLUE + Back.WHITE + linha_menu + Style.RESET_ALL)
+
+def itens_menu():
+    itens = ('1 - adicionar Novo Produto', '2 - Atualizar Produtos','3 - Visualizar Produtos existentes', '4 - Registrar e controlar vendas', '0 - Sair')
+    for item in itens:
+        print(item)
 
 def exibir_menu(): #Menu principal
-    titulo_menu("MENU")
-    print("7 - Cadastro Geral")
-    print("0 - Sair")
-#     # print("-"*70)
-    
+    titulo_menu('MENU')
+    itens_menu()
 
 
 def iniciar_sistema():
